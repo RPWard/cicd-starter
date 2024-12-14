e5d3623d5de84935427f85988bcc049b817e1e0f
Ryan Ward <PED.RWard@gmail.com>
2024-12-14 13:15:25 -0600

    added unused

--- main.go
+++ main.go
@@ -24,7 +24,11 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
-func main(){
+func unused() {
+
+}
+
+func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
