3549d4b459274f462282a197c1479dadd49a6312
Ryan Ward <PED.RWard@gmail.com>
2024-12-15 08:31:45 -0600

    fix

--- json.go
+++ json.go
@@ -28,4 +28,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	}
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("Error writing headers")
+		return
+	}
 }
